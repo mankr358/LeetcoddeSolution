5826fe1ddeae6d3711660ade7956499135b57d6b
mankr358 <134913352+mankr358@users.noreply.github.com>
2024-09-12 00:13:14 +0530

    Time: 6 ms (70.17%), Space: 47.6 MB (47.75%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -1,17 +1,20 @@
+
 class Solution {
     public void wiggleSort(int[] nums) {
+        int n = nums.length;
         int[] sorted = nums.clone();
         Arrays.sort(sorted);
 
-        int mid = (nums.length + 1) / 2;
-        int j = mid - 1;
-        int k = nums.length - 1;
+        int mid = (n + 1) / 2;  // The middle index of the smaller half
+        int j = mid - 1;        // Points to the end of the smaller half
+        int k = n - 1;          // Points to the end of the larger half
 
-        for (int i = 0; i < nums.length; i++) {
+        // Rearranging the sorted array in wiggle fashion
+        for (int i = 0; i < n; i++) {
             if (i % 2 == 0) {
-                nums[i] = sorted[j--];
+                nums[i] = sorted[j--];  // Even index, assign from smaller half
             } else {
-                nums[i] = sorted[k--];
+                nums[i] = sorted[k--];  // Odd index, assign from larger half
             }
         }
     }
