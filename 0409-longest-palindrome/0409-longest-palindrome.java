d1b4f3f83ebad7f022d4710aee9876c38bee3939
mankr358 <134913352+mankr358@users.noreply.github.com>
2024-09-14 11:41:06 +0530

    Time: 5 ms (66.71%), Space: 42.1 MB (16.11%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,15 +1,26 @@
 class Solution {
     public int longestPalindrome(String s) {
-        int [] count = new int [128];
-        for(char ch : s.toCharArray())
-        count[ch]++;
-        int res = 0;
-        for(int i =0; i< 128;i++){
-            int val = count[i];
-            res = res + (val/2) *2;
-            if (res %2 == 0 && val %2 == 1)
-            res++;
+        HashSet<Character> charSet = new HashSet<>();
+        int palindromeLength = 0;
+
+        for (char c : s.toCharArray()) {
+            // If character is already in the set, it means we have seen it before
+            if (charSet.contains(c)) {
+                // Remove it from the set (this pair can contribute to the palindrome)
+                charSet.remove(c);
+                // Increase the palindrome length by 2 (this pair is added to both ends)
+                palindromeLength += 2;
+            } else {
+                // If character is not in the set, add it (potential for future pair)
+                charSet.add(c);
+            }
         }
-        return res;
+
+        // If there are characters left in the set, we can place one in the middle
+        if (!charSet.isEmpty()) {
+            palindromeLength++;
+        }
+
+        return palindromeLength;
     }
 }
