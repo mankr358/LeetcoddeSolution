52e4c018751a1b6af75065b56336ce099f635672
mankr358 <134913352+mankr358@users.noreply.github.com>
2024-09-19 11:01:28 +0530

    Time: 1 ms (94.56%), Space: 44.4 MB (50.77%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,20 +1,23 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        backtracking(result, new ArrayList<>(), nums);  // Fix: Correct method call with result and tempList
+        boolean[] used = new boolean[nums.length];  // Boolean array to track used elements
+        backtracking(result, new ArrayList<>(), nums, used); 
         return result;  
     }
 
-    private void backtracking(List<List<Integer>> resultList, List<Integer> tempList, int[] nums) {
+    private void backtracking(List<List<Integer>> resultList, List<Integer> tempList, int[] nums, boolean[] used) {
         if (tempList.size() == nums.length) {
             resultList.add(new ArrayList<>(tempList)); 
             return;
         }
-        for (int number : nums) {
-            if (tempList.contains(number))  
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i])  // If number is already used, skip
                 continue;
-            tempList.add(number);  
-            backtracking(resultList, tempList, nums);
+            used[i] = true;
+            tempList.add(nums[i]);  
+            backtracking(resultList, tempList, nums, used);
+            used[i] = false;  // Backtrack and mark the number as unused
             tempList.remove(tempList.size() - 1);
         }
     }
