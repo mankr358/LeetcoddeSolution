ea6ce150c4b84f096a0ed6a07817fcfe0f61b080
mankr358 <134913352+mankr358@users.noreply.github.com>
2024-09-24 18:32:08 +0530

    Time: 3 ms (93.13%), Space: 43.1 MB (85.76%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,20 +1,31 @@
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.ArrayDeque;
+
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] res = new int[nums1.length];
-        HashMap<Integer,Integer> map = new HashMap<>();
-        Stack<Integer> stack = new Stack();
-        for(int i = 0;i<nums2.length;i++){
-            while(!stack.isEmpty() && stack.peek() < nums2[i]){
-                map.put(stack.pop(),nums2[i]);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>(); // More efficient stack implementation
+
+        // Traverse through nums2 to populate the map with next greater elements
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
             }
-            stack.push(nums2[i]);
+            stack.push(num);
         }
-        for(int i : stack){
-            map.put(i,-1);
+
+        // Elements left in the stack have no greater element, map them to -1
+        while (!stack.isEmpty()) {
+            map.put(stack.pop(), -1);
         }
-        for(int i = 0; i<nums1.length;i++){
+
+        // Build the result array based on the mapped next greater elements
+        for (int i = 0; i < nums1.length; i++) {
             res[i] = map.get(nums1[i]);
         }
+
         return res;
     }
 }
