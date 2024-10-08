8a8b0af781e889ad70bcfc4c42d5f8f957541df5
mankr358 <134913352+mankr358@users.noreply.github.com>
2024-10-08 18:17:25 +0530

    Time: 0 ms (100%), Space: 50.2 MB (54.11%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,9 +1,33 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-       int xor_value =0;
-       for(int num :  nums){
-       xor_value ^= num; 
-       }
-       return xor_value;
+        int n = nums.length;
+        int l = 0, r = n - 1;
+        int mid;
+        
+        while (l < r) {
+            mid = l + (r - l) / 2;
+            boolean isEven = (r - mid) % 2 == 0;
+
+            // Check if mid is equal to the next element
+            if (nums[mid] == nums[mid + 1]) {
+                if (isEven) {
+                    l = mid + 2;  // Move left pointer
+                } else {
+                    r = mid - 1;  // Move right pointer
+                }
+            } 
+            // Check if mid is equal to the previous element
+            else if (nums[mid] == nums[mid - 1]) {
+                if (isEven) {
+                    r = mid - 2;  // Move right pointer
+                } else {
+                    l = mid + 1;  // Move left pointer
+                }
+            } else {
+                return nums[mid];  // Single element found
+            }
+        }
+        
+        return nums[l];  // The single element will be at l (or r)
     }
 }
