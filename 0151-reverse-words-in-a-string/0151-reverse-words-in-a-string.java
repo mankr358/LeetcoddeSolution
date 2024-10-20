04ea92454ece50590f89c6789c61e1387bd22548
mankr358 <134913352+mankr358@users.noreply.github.com>
2024-10-20 09:06:21 +0530

    Time: 5 ms (93.27%), Space: 43.2 MB (50.26%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,52 +1,13 @@
 class Solution {
     public String reverseWords(String s) {
-        // Step 1: Trim the string and reduce multiple spaces to single spaces
-        s = s.trim().replaceAll("\\s+", " ");
-        
-        // Convert the string to a character array for in-place modifications
-        char[] strArray = s.toCharArray();
-        
-        // Step 2: Reverse the entire string
-        reverse(strArray, 0, strArray.length - 1);
-        
-        // Step 3: Reverse each word in the reversed string
-        reverseWordsInArray(strArray);
-        
-        // Convert the character array back to a string and return
-        return new String(strArray);
-    }
-    
-    // Function to reverse a part of the character array using two pointers
-    private void reverse(char[] arr, int start, int end) {
-        while (start < end) {
-            // Swap the characters at start and end
-            char temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            
-            // Move the pointers towards the center
-            start++;
-            end--;
-        }
-    }
-    
-    // Function to reverse each word in the character array
-    private void reverseWordsInArray(char[] arr) {
-        int n = arr.length;
-        int start = 0, end = 0;
-        
-        while (start < n) {
-            // Move end pointer to the end of the current word
-            while (end < n && arr[end] != ' ') {
-                end++;
+        String[] words = s.trim().split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = words.length-1;i >=0; i--){
+            if(words[i].length() >0){
+                sb.append(words[i]);
+                sb.append(' ');
             }
-            
-            // Reverse the current word
-            reverse(arr, start, end - 1);
-            
-            // Move both pointers to the next word
-            start = end + 1;
-            end = start;
         }
+        return sb.substring(0,sb.length() -1).toString();
     }
-} 
+}
