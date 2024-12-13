7ee8476ef4db52bcde92de84353d5b3bc21bb412
mankr358 <134913352+mankr358@users.noreply.github.com>
2024-12-13 23:06:21 +0530

    Time: 27 ms (84.3%), Space: 51.4 MB (57.72%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,38 +3,38 @@ class Solution {
         if (nums.length < 3) {
             return new ArrayList<>();
         }
-
         List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums);
+        Arrays.sort(nums); // Sort the array
 
         for (int i = 0; i < nums.length - 2; i++) {
             if (i != 0 && nums[i] == nums[i - 1]) {
-                continue;
+                continue; // Skip duplicates
             }
             twoSum(nums, i + 1, result, -nums[i]);
         }
-
         return result;
     }
 
     private void twoSum(int[] nums, int k, List<List<Integer>> result, int target) {
-        int i = k, j = nums.length - 1;
+        int i = k;
+        int j = nums.length - 1;
 
         while (i < j) {
-            if (nums[i] + nums[j] > target) {
-                j--;
-            } else if (nums[i] + nums[j] < target) {
-                i++;
+            int sum = nums[i] + nums[j]; // Recalculate sum in each iteration
+
+            if (sum < target) {
+                i++; // Move left pointer to the right
+            } else if (sum > target) {
+                j--; // Move right pointer to the left
             } else {
                 result.add(Arrays.asList(-target, nums[i], nums[j]));
-
+                // Skip duplicates for both pointers
                 while (i < j && nums[i] == nums[i + 1]) {
                     i++;
                 }
                 while (i < j && nums[j] == nums[j - 1]) {
                     j--;
                 }
-
                 i++;
                 j--;
             }
