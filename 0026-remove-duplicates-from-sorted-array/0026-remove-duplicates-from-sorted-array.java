21bbfd6d6e454413331c7afb4fc8dd9b3a3b946e
mankr358 <134913352+mankr358@users.noreply.github.com>
2024-12-15 00:59:58 +0530

    Time: 1 ms (66.09%), Space: 44.9 MB (35.04%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,17 +1,27 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
+        // if (nums == null || nums.length == 0) {
+        //     return 0;
+        // }
+        // int i = 0; // Pointer for the last unique element
+        // for (int j = 1; j < nums.length; j++) { // Start from the second element
+        //     if (nums[i] != nums[j]) { // Check if the current element is different
+        //         i++; // Move to the next position for the unique element
+        //         nums[i] = nums[j]; // Update the position with the unique element
+        //     }
+        // }
         
-        int i = 0; // Pointer for the last unique element
-        for (int j = 1; j < nums.length; j++) { // Start from the second element
-            if (nums[i] != nums[j]) { // Check if the current element is different
-                i++; // Move to the next position for the unique element
-                nums[i] = nums[j]; // Update the position with the unique element
+        // return i + 1; // Length of the unique elements
+if(nums.length == 0){
+    return 0;
+}
+        int i = 0;
+        for(int  j=1;j<nums.length;j++ ){
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
             }
         }
-        
-        return i + 1; // Length of the unique elements
+        return i+1;
     }
 }
