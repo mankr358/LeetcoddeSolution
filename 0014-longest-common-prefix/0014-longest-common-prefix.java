4b75c8590cd4c443db58e755b77bbbe61b955e37
mankr358 <134913352+mankr358@users.noreply.github.com>
2024-12-25 07:33:18 +0530

    Time: 1 ms (64.36%), Space: 41.7 MB (35.23%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,21 +1,30 @@
 class Solution {
-    public String common(String s1,String s2) {
-        int n = Math.min(s1.length(),s2.length());
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0;i<n;i++){
-            if(s1.charAt(i)==s2.charAt(i)){
-                sb.append(s1.charAt(i));
-            }else
-            break;
+    // Method to find the common prefix between two strings
+    public String common(String s1, String s2) {
+        int n = Math.min(s1.length(), s2.length()); // Take the smaller length
+        StringBuilder sb = new StringBuilder(); // For building the common prefix
+        
+        for (int i = 0; i < n; i++) { // Compare characters
+            if (s1.charAt(i) == s2.charAt(i)) {
+                sb.append(s1.charAt(i)); // Append if characters match
+            } else {
+                break; // Stop when characters differ
+            }
         }
-        return sb.toString();
-    }
-    public String longestCommonPrefix(String[] strs){
-        String res= strs[0];
-        for(int i =1;i<strs.length;i++){
-            res = common(res,strs[i]);
-        }
-        return res;
+        return sb.toString(); // Return the common prefix
     }
 
+    // Method to find the longest common prefix in an array of strings
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return ""; // Handle edge cases
+        }
+        
+        String res = strs[0]; // Start with the first string
+        
+        for (int i = 1; i < strs.length; i++) { // Compare with each string
+            res = common(res, strs[i]);
+        }
+        return res; // Return the final longest common prefix
+    }
 }
