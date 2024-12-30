8f0515f671de24f9db0097db2212d41617deca2d
mankr358 <134913352+mankr358@users.noreply.github.com>
2024-12-30 17:20:21 +0530

    Time: 0 ms (100%), Space: 43.4 MB (8.27%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,47 +1,49 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-         int n = nums.length;
+       int n = nums.length;
+    int gola_index = -1;
 
-        // Step 1: Find the first index from the end where nums[i] > nums[i-1]
-        int i = n - 1;
-        while (i > 0 && nums[i] <= nums[i - 1]) {
-            i--;
+    // Step 1: Find the first decreasing pair from the right
+    for (int i = n - 1; i > 0; i--) {
+        if (nums[i] > nums[i - 1]) {
+            gola_index = i - 1;
+            break;
         }
+    }
 
-        // Step 2: If the sequence is not in descending order
-        if (i > 0) {
-            int index = i;
+    // If a decreasing pair was found
+    if (gola_index != -1) {
+        int swap_index = gola_index;
 
-            // Step 3: Find the first number larger than nums[i-1]
-            for (int j = n - 1; j >= i; j--) {
-                if (nums[j] > nums[i - 1]) {
-                    index = j;
-                    break;
-                }
+        // Step 2: Find the element just larger than nums[gola_index]
+        for (int j = n - 1; j > gola_index; j--) {
+            if (nums[j] > nums[gola_index]) {
+                swap_index = j;
+                break;
             }
-
-            // Step 4: Swap nums[i-1] with the number found at 'index'
-            swap(nums, i - 1, index);
         }
 
-        // Step 5: Reverse the sequence after the index 'i'
-        reverse(nums, i, n - 1);
+        // Step 3: Swap the elements
+        swap(nums, gola_index, swap_index);
     }
 
-    // Helper function to swap two elements
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
+    // Step 4: Reverse the part of the array after gola_index
+    reverse(nums, gola_index + 1, n - 1);
+}
 
-    // Helper function to reverse an array from index 'start' to 'end'
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            swap(nums, start, end);
-            start++;
-            end--;
-        
+// Helper function to swap two elements
+private void swap(int[] nums, int i, int j) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+// Helper function to reverse the array from index 'start' to 'end'
+private void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+        swap(nums, start, end);
+        start++;
+        end--;
     }
 }
 }
