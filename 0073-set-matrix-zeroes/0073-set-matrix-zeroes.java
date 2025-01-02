e02aa82e37b26918ba1890dee33750f565f55c41
mankr358 <134913352+mankr358@users.noreply.github.com>
2025-01-02 16:14:57 +0530

    Time: 1 ms (73.88%), Space: 45.5 MB (70.71%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,30 +1,60 @@
 class Solution {
    public void setZeroes(int[][] matrix) {
-    boolean firstRow = false, firstCol = false;
+     int n = matrix.length;
+        int m = matrix[0].length;
 
-    // Set markers in first row and first column
-    for (int i = 0; i < matrix.length; i++)
-      for (int j = 0; j < matrix[0].length; j++)
-        if (matrix[i][j] == 0) {
-          if (i == 0) firstRow = true;
-          if (j == 0) firstCol = true;
-          matrix[0][j] = 0;
-          matrix[i][0] = 0;
+        boolean row = false, col = false;
+
+        // Check if the first column should be set to zero
+        for (int i = 0; i < n; i++) 
+            if (matrix[i][0] == 0) 
+                col = true;
+
+        // Check if the first row should be set to zero
+        for (int j = 0; j < m; j++) 
+            if (matrix[0][j] == 0) 
+                row = true;
+
+        // Use first row and column as markers
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
         }
 
-    // Replace inner matrix
-    for (int i = 1; i < matrix.length; i++)
-      for (int j = 1; j < matrix[0].length; j++)
-        if (matrix[i][0] == 0 || matrix[0][j] == 0)
-          matrix[i][j] = 0;
+        // Set rows based on markers
+        for (int i = 1; i < n; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 0; j < m; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
 
-    // Last remaining checks
-    if (firstRow)
-      for (int j = 0; j < matrix[0].length; j++)
-        matrix[0][j] = 0;
+        // Set columns based on markers
+        for (int j = 1; j < m; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 0; i < n; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
 
-    if (firstCol)
-      for (int i = 0; i < matrix.length; i++)
-        matrix[i][0] = 0; 
+        // Set the first row if needed
+        if (row) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Set the first column if needed
+        if (col) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
