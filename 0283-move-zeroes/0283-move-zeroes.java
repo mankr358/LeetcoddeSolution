a29006d0b358e29d698f3955ae1399f9fb34ab4f
mankr358 <134913352+mankr358@users.noreply.github.com>
2025-01-11 17:13:06 +0530

    Time: 2 ms (85.22%), Space: 46 MB (41.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,36 +1,18 @@
-// class Solution {
-//     public void moveZeroes(int[] nums) {
-//         int i = 0; // Pointer for the next non-zero element
-//         for (int j = 0; j < nums.length; j++) {
-//             if (nums[j] != 0) { // Only swap when nums[j] is non-zero
-//                 swap(nums, i, j);
-//                 i++; // Increment i only after a successful swap
-//             }
-//         }
-//     }
-
-//     // Swap two elements in the array
-//     public void swap(int[] arr, int i, int j) {
-//         int temp = arr[i];
-//         arr[i] = arr[j];
-//         arr[j] = temp;
-//     }
-// }
-
 class Solution {
     public void moveZeroes(int[] nums) {
-     int i=0;
-     for(int j = 0; j< nums.length;j++){
-        if(nums[j] != 0){
-            swap(nums,i,j);
-            i++;
+        int i = 0; // Pointer for the next non-zero element
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) { // Only swap when nums[j] is non-zero
+                swap(nums, i, j);
+                i++; // Increment i only after a successful swap
+            }
         }
-        
-     }
     }
-    public void swap(int[] nums,int i ,int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+
+    // Swap two elements in the array
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
